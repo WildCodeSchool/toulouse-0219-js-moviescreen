2f9d3e2913c115e53b75280863e9dcb7be84b450
Marta Fumagalli <fumagalli.marta@gmail.com>
2019-04-02 17:04:48 +0200

    details-movie-card using json file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DetailsMovieCard from './components/details/DetailsMovieCard'
 import './App.css';
 import './components/details/DetailsMovieCard.css';
+import popular from './popular-movies.json';
 
 
 const movies = [
@@ -12,25 +13,13 @@ const movies = [
     genre01 : "Animation",
     genre02 : "Family",
     genre03 : "Adventure",
-    synopsis : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-    button : "aaaaaa",
+    synopsis : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat...",
+    button1 : "http://www.google.com",
+    button2 : "http://www.google.com",
     date : "February 22, 2019",
     status : "Released",
+    trailer : "http://www.google.com",
   },
-
-  { 
-    picture : "http://lorempixel.com/800/950/nature/4",
-    title : "BBBB BBBBB",
-    description : "bbbb bbbb bbbbbbbbbbbb bbbbbb",
-    button : "bbbbbbb",
-    stella : true
-  },
-  { 
-    picture : "http://lorempixel.com/800/950/nature/5",
-    title : "CCCCC CC CCC",
-    description : "cccc cc ccccccc cccc ccccccc ccc",
-    button : "cccccc"
-  }
 ];
 
 class App extends Component {
@@ -39,7 +28,7 @@ class App extends Component {
       <div>
         <div className="row">
           <div className="container">
-            <DetailsMovieCard  {...movies[0]} />
+            <DetailsMovieCard  {...popular.results[0]} {...movies[0]} />
           </div>          
         </div>
       </div>
--- src/components/details/DetailsMovieCard.js
+++ src/components/details/DetailsMovieCard.js
@@ -1,30 +1,28 @@
 import React, { Component } from 'react'
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class DetailsMovieCard extends Component {
   render() {
       
-    const {picture, title, synopsis, button, director, genre01, genre02, genre03, date, status} = this.props;
+    const {poster_path, original_title, overview, button1, button2, director, genre_ids, release_date, status, video} = this.props;
 
     return (
     <div>
       <div className="card m-4">
         <div className="row no-gutters m-3">
           <div className="col-md-4 col-lg-6">
-            <img src={picture} className="card-img my-card-img" alt="" />
+            <img src={"https://image.tmdb.org/t/p/w600_and_h900_bestv2" + poster_path} className="card-img my-card-img" alt="" /><a href={video} target="_blank"><img src="https://cdn1.iconfinder.com/data/icons/social-media-glossy/512/7-player_windows_media_player_video_social-512.png" className="my-video-player"/></a>
           </div>
           <div className="col-md-8 col-lg-6 p-2">
-            <div className="card-body my-card-body p-0 pl-3">
-              <h2 className="card-title pb-4">{title}</h2>
+            <div className="card-body my-card-body p-0 pl-lg-3">
+              <h2 className="card-title pb-4">{original_title}</h2>
               <h5 className="pb-4">
-                Genre: <span className="my-genre">{genre01}</span><span className="my-genre">{genre02}</span><span className="my-genre">{genre03}</span></h5>
+                Genre: <span className="my-genre">{genre_ids}</span><span className="my-genre">{genre_ids}</span><span className="my-genre">{genre_ids}</span></h5>
               <p>Directed by: <span className='font-weight-bold'>{director}</span></p>             
               <p>Status: <span className='font-weight-bold'>{status}</span></p>
-              <p className="card-text"><small className="text-muted">Release date: {date}</small></p>
-              <p className="card-text">{synopsis}</p>              
-              <a href="http://www.google.com" className="btn btn-primary my-btn my-3">Add to my favorits</a>
-              <a href="http://www.google.com" className="btn btn-primary my-btn mt-3">Add to watch later</a>
+              <p className="card-text"><small className="text-muted">Release date: {release_date}</small></p>
+              <p className="card-text pb-2">{overview}</p>              
+              <a href={button1} className="btn btn-primary my-btn my-3">Add to my favorits</a>
+              <a href={button2} className="btn btn-primary my-btn mt-3">Add to watch later</a>
             </div>
           </div>
         </div>
