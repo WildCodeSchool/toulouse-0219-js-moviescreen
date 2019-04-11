b19626622939f7f1cc6abdd7d52b48b80746e42a
Anisse Mahtat <anissemahtat@yahoo.com>
2019-04-11 15:26:06 +0200

    bon code fav

--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
 import movies from '../popular-movies';
 
 const FavoriteList = props => (
@@ -6,9 +8,19 @@ const FavoriteList = props => (
     {
       props.movies.map(
         movie => {
-          return (<div key={movie.popularity}>
-            {movie.popularity}
-          </div>);
+          return (
+<div className="container img-container" key={movie.popularity}>
+            <Card>
+              <CardImg width="150vh" height="15vh" top src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.backdrop_path}`} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{movie.original_title}</CardTitle>
+          <CardSubtitle>{movie.tagline}</CardSubtitle>
+          <CardText>{movie.overview}</CardText>
+          
+        </CardBody>
+      </Card>
+          </div>
+          );
         }
       )
     }
