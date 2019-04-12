409ea3a74798c0f7c29e9fdf545397a5706aacf8
Arnaud Bourrounet <arnaud.bourrounet@gmail.com>
2019-04-12 11:05:36 +0200

    add link router in navbar

--- src/App.js
+++ src/App.js
@@ -40,8 +40,8 @@ class App extends Component {
         </header>
 
         <Route path="/" exact component={Homepage} />
-        <Route path="/MovieDetails/:id" exact component={MovieDetails} />
-        <Route path="/favorite-and-other" render={(props) => <Favorite {...props} favorites={this.state.favorites}/>} />
+        <Route path="/movie-details/:id" exact component={MovieDetails} />
+        <Route path="/favorites" render={(props) => <Favorite {...props} favorites={this.state.favorites}/>} />
         <footer className="foot">
           <Footer />
         </footer>
