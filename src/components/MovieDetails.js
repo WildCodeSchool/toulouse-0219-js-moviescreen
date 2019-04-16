5ff8be685ca6cb83106daf009e50105643feda6a
Arnaud Bourrounet <arnaud.bourrounet@gmail.com>
2019-04-16 13:58:03 +0200

    no change

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -88,9 +88,7 @@ class MovieDetails extends Component {
           <CastingCard casting={this.state.casting} />
           <h2>Trailer</h2>
           <Player trailer={this.state.trailer} />
-            
         </div>
-
       </div>
     );
   }
